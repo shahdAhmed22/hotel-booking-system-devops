6b659395d7cf051f07f57dcee8d59e6d3f5ec9d3
Marvel9877 <marvelhelmy9@gmail.com>
2025-11-16 13:23:24 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,29 +117,42 @@ resource "null_resource" "wait_for_cluster" {
   depends_on = [null_resource.configure_kubectl]
 }
 
-# Get cluster info AFTER kubectl is configured
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-  depends_on = [null_resource.wait_for_cluster]
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-  depends_on = [null_resource.wait_for_cluster]
-}
-
-# Kubernetes provider - configured after kubectl setup
+# Kubernetes provider - using exec for authentication (works better than token)
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name,
+      "--region",
+      var.aws_region
+    ]
+  }
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.cluster.token
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args = [
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name,
+        "--region",
+        var.aws_region
+      ]
+    }
   }
 }
 
